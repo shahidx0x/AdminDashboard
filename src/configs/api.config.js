bb272a3b5cc77d70987ab83f3bdc0c63d23a52d0
shahidx0x <shahid.dev.cse@gmail.com>
2023-10-10 21:48:20 +0600

    session added

--- src/configs/api.config.js
+++ src/configs/api.config.js
@@ -2,6 +2,7 @@ let host = "https://rest.app.gobd.xyz";
 export const config = {
   endpoints: {
     login: host + "/signin",
+    session_check: host + "/verify/session",
     users: host + "/get/users", // has query params -> see api doc
     search_user: host + "/get/user", // has query params -> see api doc
     update_user: host + "/user/info/update",
