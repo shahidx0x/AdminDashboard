ea2b4dd387bababb41550bd96d66a50bac7c2753
shahidx0x <shahid.dev.cse@gmail.com>
2023-11-24 07:07:20 +0600

    notifiaction upload

--- src/pages/Dashbord/Notification/Notification.jsx
+++ src/pages/Dashbord/Notification/Notification.jsx
@@ -19,11 +19,6 @@ export default function Notification() {
   const [hover, setHover] = useState(true);
   const user = useSelector((state) => state.user.user);
 
-  const [inputValue, setInputValue] = useState();
-  const [isSearching, setIsSearching] = useState(false);
-  const [stockValue, setStockValue] = useState(null);
-  const [expandedRowKeys, setExpandedRowKeys] = useState([]);
-
   const {
     data,
     status,
@@ -40,7 +35,6 @@ export default function Notification() {
   );
 
   const TextCell = ({ rowData, dataKey, ...props }) => {
-    console.log(rowData);
     return (
       <Cell {...props}>
         <div>
@@ -93,7 +87,7 @@ export default function Notification() {
             loading={status === "loading" ? true : false}
             height={600}
             hover={hover}
-            showHeader={false}
+            showHeader={true}
             autoHeight={false}
             data={displayedData}
             bordered={bordered}
@@ -120,91 +114,6 @@ export default function Notification() {
           </Table>
         </div>
       </div>
-      {/* <div className="border-b">
-        <div className="flex items-center justify-center py-10 lg:px-0 sm:px-6 px-4">
-          <div className="lg:w-3/5 w-full  flex items-center justify-between border-t border-gray-200">
-            <div className="flex items-center pt-3 text-gray-600 hover:text-indigo-700 cursor-pointer">
-              <svg
-                width={14}
-                height={8}
-                viewBox="0 0 14 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1.1665 4H12.8332"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M1.1665 4L4.49984 7.33333"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M1.1665 4.00002L4.49984 0.666687"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <p
-                onClick={handleLoadPrevious}
-                className="text-sm ml-3 font-medium leading-none "
-              >
-                Previous
-              </p>
-            </div>
-            <div className="sm:flex hidden">
-              <p className="text-sm font-bold leading-none cursor-pointer text-gray-600 hover:text-indigo-700 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">
-                pages : {page}/{data?.meta?.total_page}
-              </p>
-            </div>
-            <div className="flex items-center pt-3 text-gray-600 hover:text-indigo-700 cursor-pointer">
-              <p
-                onClick={handleLoadMore}
-                className="text-sm font-medium leading-none mr-3"
-              >
-                Next
-              </p>
-              <svg
-                width={14}
-                height={8}
-                viewBox="0 0 14 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1.1665 4H12.8332"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M9.5 7.33333L12.8333 4"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M9.5 0.666687L12.8333 4.00002"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
