d0e59ae0625188bd8b6591253e9636dfb428c278
shahidx0x <dev.imshahid@gmail.com>
2023-12-18 14:44:37 +0600

    update 5 files and delete 22 files

--- src/api/UnitType.js
+++ src/api/UnitType.js
@@ -2,95 +2,171 @@ import axios from "axios";
 import { config } from "../configs/api.config";
 
 export async function getUnit(data) {
-    const headers = {
-      "Cache-Control": "no-cache",
-      Authorization: `Bearer ${data.queryKey[2]}`,
-    };
-  
-    try {
-      const response = await axios.get(
-        config.endpoints.host + `/unit-types`,
-        { headers }
-      );
-  
-      return response;
-    } catch (error) {
-      console.error(
-        "Error fetching unit:",
-        error.response ? error.response.data : error.message
-      );
-      throw error;
-    }
+  const headers = {
+    "Cache-Control": "no-cache",
+    Authorization: `Bearer ${data.queryKey[2]}`,
+  };
+
+  try {
+    const response = await axios.get(config.endpoints.host + `/unit-types`, {
+      headers,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(
+      "Error fetching unit:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
 }
-  
+
+export async function getUnitInfo(data) {
+  const headers = {
+    "Cache-Control": "no-cache",
+    Authorization: `Bearer ${data.queryKey[2]}`,
+  };
+
+  try {
+    const response = await axios.get(config.endpoints.host + `/units`, {
+      headers,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(
+      "Error fetching unit info:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+}
+
 export async function createUnit({ data, token }) {
-    try {
-      const headers = {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      };
-  
-      const response = await axios.post(config.endpoints.host + '/unit-types', data, {
-        headers,
-      });
-  
-      return response;
-    } catch (error) {
-      if (error.response) {
-        console.error("Server Error:", error.response.data);
-      } else if (error.request) {
-        console.error("No response received from the server.");
-      } else {
-        console.error("Error:", error.message);
-      }
-  
-      throw error;
-    }
-}
-  
-export async function deleteUnit(data) {
-    try {
-      const headers = {
-        Authorization: `Bearer ${data?.token}`,
-        "Content-Type": "application/json",
-      };
-  
-      const response = await axios.delete(
-        config.endpoints.host + `/unit-types/${data?.deleteId}`,
-        {
-          headers,
-        }
-      );
-  
-      return response;
-    } catch (error) {
-      if (error.response) {
-        console.error("Server Error:", error.response.data);
-      } else if (error.request) {
-        console.error("No response received from the server.");
-      } else {
-        console.error("Error:", error.message);
-      }
-  
-      throw error;
-    }
-}
-  
-export async function updateUnit({ data, token, id }) {
+  try {
     const headers = {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     };
-    const response = await axios.patch(
-      config.endpoints.host + `/unit-types/${id}`,
+
+    const response = await axios.post(
+      config.endpoints.host + "/unit-types",
       data,
       {
         headers,
       }
     );
-  
-    if (response.status !== 200) {
-      throw new Error("Error updating unit");
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received from the server.");
+    } else {
+      console.error("Error:", error.message);
     }
-    return response.data;
+
+    throw error;
   }
+}
+
+export async function createUnitInfo({ data, token }) {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+
+    const response = await axios.post(config.endpoints.host + "/units", data, {
+      headers,
+    });
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received from the server.");
+    } else {
+      console.error("Error:", error.message);
+    }
+
+    throw error;
+  }
+}
+
+export async function deleteUnit(data) {
+ 
+  try {
+    const headers = {
+      Authorization: `Bearer ${data?.token}`,
+      "Content-Type": "application/json",
+    };
+
+    const response = await axios.delete(
+      config.endpoints.host + `/unit-types/${data?.id}`,
+      {
+        headers,
+      }
+    );
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received from the server.");
+    } else {
+      console.error("Error:", error.message);
+    }
+    throw error;
+  }
+}
+
+export async function deleteUnitInfo(data) {
+
+  try {
+    const headers = {
+      Authorization: `Bearer ${data?.token}`,
+      "Content-Type": "application/json",
+    };
+
+    const response = await axios.delete(
+      config.endpoints.host + `/units/${data?.deleteId}`,
+      {
+        headers,
+      }
+    );
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received from the server.");
+    } else {
+      console.error("Error:", error.message);
+    }
+    throw error;
+  }
+}
+
+export async function updateUnit({ data, token, id }) {
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+  const response = await axios.patch(
+    config.endpoints.host + `/unit-types/${id}`,
+    data,
+    {
+      headers,
+    }
+  );
+
+  if (response.status !== 200) {
+    throw new Error("Error updating unit");
+  }
+  return response.data;
+}
--- src/pages/Dashbord/UniteType/UniteType.jsx
+++ src/pages/Dashbord/UniteType/UniteType.jsx
@@ -15,8 +15,14 @@ import {
   toaster,
 } from "rsuite";
 
-
-import { createUnit, deleteUnit, getUnit } from "../../../api/UnitType";
+import {
+  createUnit,
+  createUnitInfo,
+  deleteUnit,
+  deleteUnitInfo,
+  getUnit,
+  getUnitInfo,
+} from "../../../api/UnitType";
 const { Column, HeaderCell, Cell } = Table;
 const rowKey = "_id";
 
@@ -71,14 +77,15 @@ const renderRowExpanded = (rowData) => {
 };
 
 export default function UniteType() {
-
   const user = useSelector((state) => state.user.user);
   const settings = useSelector((state) => state.settings);
   const [expandedRowKeys] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openUnit, setOpenUnit] = useState(false);
   const [confirm, setConfirm] = useState(false);
   const handleClose = () => setOpen(false);
   const [unitValue, setUnitValue] = useState(null);
+  const [unitLabel, setUnitLabel] = useState(null);
   const handleConfirm = (e) => {
     if (e.target.value === "confirm") {
       setConfirm(true);
@@ -87,31 +94,38 @@ export default function UniteType() {
     }
   };
   const [deleteId, setDeleteId] = useState(null);
-  const handleOpen = () => {
+
+  const handleOpenUnit = () => {
     setOpen(true);
   };
 
-  const {
-    data,
-    status,
-    refetch,
-  } = useQuery(["units", user.jwt], getUnit, {
+  const { data, status, refetch } = useQuery(["units", user.jwt], getUnit, {
     cacheTime: 0,
   });
 
-  const unit_type = data?.data.map(
-    item => ({ label: item.label, value: item.value })
-  );
+  const {
+    data: utinfo,
+    refetch: utinfo_refetch,
+  } = useQuery(["unit-info", user.jwt], getUnitInfo, {
+    cacheTime: 0,
+  });
+
+  const unit_type = data?.data.map((item) => ({
+    label: item.label,
+    value: item.value,
+  }));
+
   const mutation_delete = useMutation(deleteUnit);
-  const handleOk = () => {
-    mutation_delete.mutate(
+  const mutation_delete_unitinfo = useMutation(deleteUnitInfo);
+  const handleUnitDelete = () => {
+    mutation_delete_unitinfo.mutate(
       { deleteId, token: user.jwt },
       {
         onSuccess: () => {
           toaster.push(
             <Message type="success">Unit deleted successfully</Message>
           );
-          refetch();
+          utinfo_refetch();
         },
         onError: () => {
           toaster.push(<Message type="error">Unit delete failed !</Message>);
@@ -119,12 +133,16 @@ export default function UniteType() {
       }
     );
     setOpen(false);
-    refetch();
+    utinfo_refetch();
   };
+
+
+
+
   const mutation_create = useMutation(createUnit);
   const handleAddUnit = () => {
     mutation_create.mutate(
-      { data:{"label": unitValue,"value":unitValue}, token: user.jwt },
+      { data: { label: unitValue, value: unitValue }, token: user.jwt },
       {
         onSuccess: () => {
           toaster.push(
@@ -134,19 +152,44 @@ export default function UniteType() {
         },
         onError: (error) => {
           console.log(error);
-          toaster.push(<Message type="error">{ error.response.data.message}</Message>);
+          toaster.push(
+            <Message type="error">{error.response.data.message}</Message>
+          );
         },
       }
     );
-  
     refetch();
   };
+  const mutation_create_unit_info = useMutation(createUnitInfo);
+  const handleAddUnitInfo = () => {
+    mutation_create_unit_info.mutate(
+      { data: { label: unitLabel, quantity: unitValue }, token: user.jwt },
+      {
+        onSuccess: () => {
+          toaster.push(
+            <Message type="success">Unit added successfully</Message>
+          );
+          utinfo_refetch();
+        },
+        onError: (error) => {
+          console.log(error);
+          toaster.push(
+            <Message type="error">{error.response.data.message}</Message>
+          );
+        },
+      }
+    );
+    utinfo_refetch();
+  };
+
   const CompactCell = (props) => <Cell {...props} style={{ padding: 4 }} />;
   const CompactHeaderCell = (props) => (
     <HeaderCell {...props} style={{ padding: 4 }}>
       <div className="flex justify-center font-bold">{props.children}</div>
     </HeaderCell>
   );
+  const CustomCell = settings.compact ? CompactCell : Cell;
+  const CustomHeaderCell = settings.compact ? CompactHeaderCell : HeaderCell;
 
   const TextCell = ({ rowData, dataKey, ...props }) => {
     return (
@@ -156,22 +199,48 @@ export default function UniteType() {
     );
   };
   const ActionsCell = ({ rowData, ...props }) => {
-
+    const id = rowData?._id;
+    const handleOk = () => {
+      mutation_delete.mutate(
+        { id, token: user.jwt },
+        {
+          onSuccess: () => {
+            toaster.push(
+              <Message type="success">Unit deleted successfully</Message>
+            );
+            refetch();
+          },
+          onError: () => {
+            toaster.push(<Message type="error">Unit delete failed !</Message>);
+          },
+        }
+      );
+      refetch();
+    };
+    return (
+      <Cell {...props}>
+        <div onClick={handleOk} className="flex justify-center gap-3">
+          <div className="text-red-500 border w-36 font-bold border-red-500 px-3 py-2 -mt-1 hover:text-white hover:bg-red-500 rounded-lg cursor-pointer">
+            Delete
+          </div>
+        </div>
+      </Cell>
+    );
+  };
+  const UnitActionsCell = ({ rowData, ...props }) => {
     const handleDelete = () => {
-      handleOpen();
+      console.log("btn-1");
       setDeleteId(rowData._id);
+      handleOpenUnit();
+      
     };
 
     return (
       <Cell {...props}>
-        <div className="flex justify-center gap-3">
-        
-          <button
-            className="text-red-500 border w-36 font-bold border-red-500 px-3 py-2 -mt-1 hover:text-white hover:bg-red-500 rounded-lg"
-            onClick={handleDelete}
-          >
+        <div onClick={() => handleDelete()} className="flex justify-center gap-3">
+          <div className="text-red-500 border w-36 font-bold border-red-500 px-3 py-2 -mt-1 hover:text-white hover:bg-red-500 rounded-lg cursor-pointer">
             Delete
-          </button>
+          </div>
         </div>
       </Cell>
     );
@@ -193,7 +262,7 @@ export default function UniteType() {
     {
       key: "action",
       label: "Action",
-      cellRenderer: ActionsCell,
+      cellRenderer: UnitActionsCell,
       width: 250,
     },
   ];
@@ -214,30 +283,42 @@ export default function UniteType() {
   ];
 
   const [columnKeys, setColumnKeys] = useState(
-    defaultColumns.map((column) => column.key)
+    defaultColumnsUnit.map((column) => column.key)
   );
-  const columns = defaultColumns.filter((column) =>
+
+  const columns = defaultColumnsUnit.filter((column) =>
     columnKeys.some((key) => key === column.key)
   );
 
   const [columnKeysUnit, setColumnKeysUnit] = useState(
     defaultColumnsUnit.map((column) => column.key)
   );
+
   const columnsUnit = defaultColumnsUnit.filter((column) =>
     columnKeysUnit.some((key) => key === column.key)
   );
 
-  const CustomCell = settings.compact ? CompactCell : Cell;
-  const CustomHeaderCell = settings.compact ? CompactHeaderCell : HeaderCell;
+  const [UnitcolumnKeys, setUnitColumnKeys] = useState(
+    defaultColumns.map((column) => column.key)
+  );
+
+  const columnsUnitInfo = defaultColumns.filter((column) =>
+    UnitcolumnKeys.some((key) => key === column.key)
+  );
+
+  const [columnKeysUnitInfo, setColumnKeysUnitInfo] = useState(
+    defaultColumns.map((column) => column.key)
+  );
 
   const displayedData = [...(data?.data || [])];
+  const displayedDataUnitInfo = [...(utinfo?.data || [])];
   refetch();
-
+  utinfo_refetch();
 
   return (
     <div>
       <Toaster />
-      <Modal open={open} onClose={handleClose}>
+      {/* <Modal open={openUnit} onClose={handleClose}>
         <Modal.Header className="p-5">
           <Modal.Title>
             Are you sure you want delete this unit type ?
@@ -256,7 +337,36 @@ export default function UniteType() {
         <Modal.Footer>
           <Button
             disabled={confirm ? false : true}
-            onClick={handleOk}
+            onClick={() => handleOk()}
+            className="bg-blue-500 w-20"
+            appearance="primary"
+          >
+            Confirm
+          </Button>
+          <Button className=" bg-red-500 text-white" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal> */}
+      <Modal open={open} onClose={handleClose}>
+        <Modal.Header className="p-5">
+          <Modal.Title>
+            Are you sure you want delete this unit information?
+          </Modal.Title>
+          <div className="flex flex-col gap-2">
+            <p className="mt-2">
+              All Category and Product will be deleted under this unit information
+            </p>
+            <Input
+              onChange={(value, event) => handleConfirm(event)}
+              placeholder="Type 'confirm' to delete"
+            />
+          </div>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button
+            disabled={confirm ? false : true}
+            onClick={handleUnitDelete}
             className="bg-blue-500 w-20"
             appearance="primary"
           >
@@ -268,7 +378,7 @@ export default function UniteType() {
         </Modal.Footer>
       </Modal>
 
-      <TagPicker
+      {/* <TagPicker
         className="h-12"
         data={defaultColumns}
         labelKey="label"
@@ -276,7 +386,7 @@ export default function UniteType() {
         value={columnKeys}
         onChange={setColumnKeys}
         cleanable={false}
-      />
+      /> */}
       <div className="flex gap-2">
         <div
           className="mt-5 ml-5 flex flex-col gap-2"
@@ -291,7 +401,7 @@ export default function UniteType() {
               hover={settings.hover}
               showHeader={settings.header}
               autoHeight={false}
-              data={displayedData}
+              data={displayedDataUnitInfo}
               bordered={settings.bordered}
               cellBordered={settings.bordered}
               headerHeight={settings.compact ? 40 : 30}
@@ -300,7 +410,7 @@ export default function UniteType() {
               expandedRowKeys={expandedRowKeys}
               renderRowExpanded={renderRowExpanded}
             >
-              {columns.map((column) => {
+              {columnsUnitInfo.map((column) => {
                 const { key, label, cellRenderer, ...rest } = column;
                 return (
                   <Column {...rest} key={key}>
@@ -319,58 +429,75 @@ export default function UniteType() {
           </div>
         </div>
         <div className="mt-5 flex flex-col gap-2 w-60">
-         
           <Table
-              shouldUpdateScroll={true}
-              rowKey={rowKey}
-              loading={status === "loading" ? true : false}
-              height={600}
-              hover={settings.hover}
-              showHeader={settings.header}
-              autoHeight={false}
-              data={displayedData}
-              bordered={settings.bordered}
-              cellBordered={settings.bordered}
-              headerHeight={settings.compact ? 40 : 30}
-              rowHeight={50}
-              rowExpandedHeight={150}
-              expandedRowKeys={expandedRowKeys}
-              renderRowExpanded={renderRowExpanded}
-            >
-              {columnsUnit.map((column) => {
-                const { key, label, cellRenderer, ...rest } = column;
-                return (
-                  <Column {...rest} key={key}>
-                    <CustomHeaderCell>{label}</CustomHeaderCell>
-                    {cellRenderer ? (
-                      React.createElement(cellRenderer, {
-                        dataKey: key,
-                      })
-                    ) : (
-                      <CustomCell dataKey={key} />
-                    )}
-                  </Column>
-                );
-              })}
+            shouldUpdateScroll={true}
+            rowKey={rowKey}
+            loading={status === "loading" ? true : false}
+            height={600}
+            hover={settings.hover}
+            showHeader={settings.header}
+            autoHeight={false}
+            data={displayedData}
+            bordered={settings.bordered}
+            cellBordered={settings.bordered}
+            headerHeight={settings.compact ? 40 : 30}
+            rowHeight={50}
+            rowExpandedHeight={150}
+            expandedRowKeys={expandedRowKeys}
+            renderRowExpanded={renderRowExpanded}
+          >
+            {columnsUnit.map((column) => {
+              const { key, label, cellRenderer, ...rest } = column;
+              return (
+                <Column {...rest} key={key}>
+                  <CustomHeaderCell>{label}</CustomHeaderCell>
+                  {cellRenderer ? (
+                    React.createElement(cellRenderer, {
+                      dataKey: key,
+                    })
+                  ) : (
+                    <CustomCell dataKey={key} />
+                  )}
+                </Column>
+              );
+            })}
           </Table>
-         
         </div>
         <div className="mt-5 flex flex-col gap-2">
-        <Input onChange={(value) => setUnitValue(value)} className="w-60" placeholder="enter unit type" />
-       
-       <Button onClick={() => handleAddUnit()} className="bg-blue-500 w-full font-bold" appearance="primary">
-         Add Unit Type
-       </Button>
-          <SelectPicker data={unit_type} onChange={(value) => setUnitValue(value)} className="w-60" placeholder="select unit type" />
-          <Input onChange={(value) => setUnitValue(value)} className="w-60" placeholder="enter a value" />
-       
-          <Button onClick={() => handleAddUnit()} className="bg-blue-500 w-full font-bold" appearance="primary">
+          <Input
+            onChange={(value) => setUnitValue(value)}
+            className="w-60"
+            placeholder="enter unit type"
+          />
+
+          <Button
+            onClick={() => handleAddUnit()}
+            className="bg-blue-500 w-full font-bold"
+            appearance="primary"
+          >
+            Add Unit Type
+          </Button>
+          <SelectPicker
+            data={unit_type}
+            onChange={(value) => setUnitLabel(value)}
+            className="w-60"
+            placeholder="select unit type"
+          />
+          <Input
+            onChange={(value) => setUnitValue(value)}
+            className="w-60"
+            placeholder="enter a value"
+          />
+
+          <Button
+            onClick={() => handleAddUnitInfo()}
+            className="bg-blue-500 w-full font-bold"
+            appearance="primary"
+          >
             Add New Unit
           </Button>
         </div>
       </div>
-
- 
     </div>
   );
 }
